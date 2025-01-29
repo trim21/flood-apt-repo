49d69245b074b57c6b7f581684aa1fc09887a55f
Trim21 <trim21.me@gmail.com>
2025-01-30 00:00:37 +0800

    重命名重定向文件并更新内容，移除哈希缓存文件，调整生成“Release”文件的逻辑以支持CI环境

--- main.py
+++ main.py
@@ -75,6 +75,11 @@ release_dir.mkdir(exist_ok=True, parents=True)
 
 cache_file_path = config.output_dir.joinpath("hash-cache.json")
 
+root = Path(__file__).parent
+config.output_dir.joinpath("_redirects").write_bytes(
+    root.joinpath("_redirects").read_bytes()
+)
+
 
 @dataclasses.dataclass(kw_only=True, slots=True, frozen=True)
 class Cache:
@@ -158,89 +163,12 @@ for release in package_cache:
         f.write(release.package)
 
 
-# packages_file = open(os.path.join(release_dir, "Packages"), "w")
-# for repo in config.repositories:
-#     print(f"{repo}:", file=sys.stderr)
-#     try:
-#         latest = requests.get(
-#             f"https://api.github.com/repos/{repo}/releases/latest", headers=headers
-#         ).json()
-#         tag_name = latest["tag_name"]
-#         for asset in latest["assets"]:
-#             if not asset["name"].endswith(".deb"):
-#                 print(f"  {asset['name']}: skipped", file=sys.stderr)
-#                 continue
-#             print(f"  {asset['name']}: download", file=sys.stderr)
-#             local_dir = os.path.join(pool_root, repo, tag_name)
-#             local_name = os.path.join(local_dir, asset["name"])
-
-#             os.makedirs(local_dir, exist_ok=True)
-#             if os.path.isfile(local_name):
-#                 print(f"    {local_name} already exists", file=sys.stderr)
-#                 continue
-
-#             resp = requests.get(asset["browser_download_url"], stream=True)
-#             if not resp.ok:
-#                 continue
-#             with open(local_name, "wb") as f:
-#                 for chunk in resp.iter_content(chunk_size=1024 * 1024):
-#                     f.write(chunk)
-#         # In CI with limited disk space, process repositories one by one
-#         if "CI" in os.environ:
-#             subprocess.run(
-#                 ["dpkg-scanpackages", "--multiversion", "."],
-#                 stdin=subprocess.DEVNULL,
-#                 stdout=packages_file,
-#                 cwd=output_dir,
-#                 check=True,
-#             )
-#             with contextlib.suppress(FileNotFoundError):
-#                 shutil.rmtree(pool_root)
-#     except Exception:
-#         traceback.print_exc()
-
-# if "CI" not in os.environ:
-#     subprocess.run(
-#         ["dpkg-scanpackages", "--multiversion", "."],
-#         stdin=subprocess.DEVNULL,
-#         stdout=packages_file,
-#         cwd=output_dir,
-#         check=True,
-#     )
-# packages_file.close()
-
-# # Split the "Packages" file by architecture
-# try:
-#     packages = {}
-#     for arch in architectures:
-#         arch_dir = os.path.join(release_dir, component, f"binary-{arch}")
-#         os.makedirs(arch_dir, exist_ok=True)
-#         packages[arch] = open(os.path.join(arch_dir, "Packages"), "w")
-
-#     buf = []
-#     arch = None
-#     with open(packages_file.name, "r") as f:
-#         for line in f:
-#             if not line.strip():
-#                 if arch in architectures:
-#                     print("".join(buf), file=packages[arch])
-#                 buf = []
-#                 arch = None
-#                 continue
-#             buf.append(line)
-#             if line.startswith("Architecture:"):
-#                 arch = line.split()[1].strip()
-#     os.remove(packages_file.name)
-# finally:
-#     for f in packages.values():
-#         f.close()
-
-
-# # Generate the "Release" file
-with release_dir.joinpath("Release").open("wb") as f:
-    subprocess.run(
-        ["apt-ftparchive", "-c", f"{config.suite}.conf", "release", release_dir],
-        stdin=subprocess.DEVNULL,
-        stdout=f,
-        check=True,
-    )
+# Generate the "Release" file
+if IS_CI:
+    with release_dir.joinpath("Release").open("wb") as f:
+        subprocess.run(
+            ["apt-ftparchive", "-c", f"{config.suite}.conf", "release", release_dir],
+            stdin=subprocess.DEVNULL,
+            stdout=f,
+            check=True,
+        )
